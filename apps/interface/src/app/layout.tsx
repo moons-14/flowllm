2a6f18c4b654e0f37e44eef63e86595d1c04b0bf
moons <moons@moons14.com>
2024-06-06 00:42:40 +0900

    init

--- apps/interface/src/app/layout.tsx
+++ apps/interface/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import 'reactflow/dist/style.css';
 import { Header } from "@/components/layout/header";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,11 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="min-h-screen flex flex-col">
-          <Header />
-          <div className="border-t flex-1 bg-red-200">
-            {children}
-          </div>
+        <Header />
+        <div className="h-[calc(100vh-40px)]">
+          {children}
         </div>
       </body>
     </html>
